916500143c68163cb29702e440f027281b6bb7ec
FamiliarStrang3r <53304126+FamiliarStrang3r@users.noreply.github.com>
2020-05-30 14:35:10 +0300

    player ui logic

---
+++ Assets/hex/Scripts/UI/PlayerElement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerElement : MonoBehaviour
+{
+    [SerializeField] private Image colorImage = null;
+    [SerializeField] private Button deleteButton = null;
+
+    private void Start()
+    {
+        colorImage.color = Random.ColorHSV();
+
+        var window = FindObjectOfType<PlayersWindow>();
+        deleteButton.onClick.AddListener(() => window.DeletePlayer(transform.GetSiblingIndex()));
+    }
+}
---
+++ Assets/hex/Scripts/UI/PlayersWindow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayersWindow : MonoBehaviour
+{
+    [SerializeField] private PlayerElement playerPrefab = null;
+    [SerializeField] private Transform playersParent = null;
+    [SerializeField] private Button addPlayerButton = null;
+
+    private List<PlayerElement> players = new List<PlayerElement>();
+
+    private void Start()
+    {
+        addPlayerButton.onClick.AddListener(AddPlayer);
+
+        for (int i = 0; i < playersParent.childCount - 1; i++)
+        {
+            Destroy(playersParent.GetChild(i).gameObject);
+        }
+
+        AddPlayer();
+    }
+
+    private void AddPlayer()
+    {
+        var element = Instantiate(playerPrefab, playersParent);
+
+        int index = playersParent.childCount - 2;
+        element.transform.SetSiblingIndex(index);
+
+        players.Add(element);
+    }
+    
+    public void DeletePlayer(int index)
+    {
+        //Debug.Log(index);
+
+        var element = players[index];
+        players.Remove(element);
+
+        Destroy(element.gameObject);
+    }
+}
